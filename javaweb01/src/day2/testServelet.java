db7500b1a7cc6aaa74b12627d94781dc76a2845f
賴紀廷 <49492695+jtlai0921@users.noreply.github.com>
2020-05-28 20:47:16 +0800

    Initial commit

---
+++ javaweb01/src/day2/testServelet.java
@@ -0,0 +1,29 @@
+package day2;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/testServelet")
+public class testServelet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+   
+    public testServelet() {
+        super();
+        
+    }
+
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("GET");
+	}
+
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("post");
+	}
+
+}
