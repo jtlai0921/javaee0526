fb45af30f367f4a2c7582b3e4c7c2de0478cf950
賴紀廷 <49492695+jtlai0921@users.noreply.github.com>
2020-07-02 21:48:57 +0800

    1

---
+++ javaweb01/src/Model/product.java
@@ -0,0 +1,51 @@
+package Model;
+
+public class product {
+	private Integer id;
+	private Integer product1;
+	private Integer product2;
+	private Integer product3;
+	private Integer sum;
+	private Integer discountprice;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public Integer getProduct1() {
+		return product1;
+	}
+	public void setProduct1(Integer product1) {
+		this.product1 = product1;
+	}
+	public Integer getProduct2() {
+		return product2;
+	}
+	public void setProduct2(Integer product2) {
+		this.product2 = product2;
+	}
+	public Integer getProduct3() {
+		return product3;
+	}
+	public void setProduct3(Integer product3) {
+		this.product3 = product3;
+	}
+	public Integer getSum() {
+		return sum;
+	}
+	public void setSum(Integer sum) {
+		this.sum = sum;
+	}
+	public Integer getDiscountprice() {
+		return discountprice;
+	}
+	public void setDiscountprice(Integer discountprice) {
+		this.discountprice = discountprice;
+	}
+	
+	
+	
+	
+
+}
